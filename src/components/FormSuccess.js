7950de445192694ef2c2df3fd775f50cfbd87c2d
hillbrianna4 <73502067+hillbrianna4@users.noreply.github.com>
2021-01-23 15:45:17 -0800

    Add files via upload

---
+++ src/components/FormSuccess.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './Form.css';
+
+const FormSuccess = () => {
+  return (
+    <div className='form-content-right'>
+      <h1 className='form-success'>Welcome!</h1>
+      
+    </div>
+  );
+};
+
+export default FormSuccess;
