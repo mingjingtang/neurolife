50692d1cded067d52267cafc80d29cd04875e29d
mingjingtang <tmjcsh@gmail.com>
2021-01-23 23:30:14 -0500

    update index.html title to Neurolife, render the new App.js to Form or DailyReading

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import "./App.css";
 import DailyReading from "./components/dailyReading";
+import Form from "./components/Form";
 
 class App extends React.Component {
   state = {
-    dailyData: [],
+    isLoggedIn: false,
+    // dailyData: [],
   };
 
   //   async componentDidMount() {}
 
   render() {
+    const { isLoggedIn } = this.state;
+
     return (
       <main>
-        <DailyReading />
+        {!isLoggedIn ? <Form isSubmmited={isLoggedIn} /> : <DailyReading />}
       </main>
     );
   }
