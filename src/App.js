a3a2bf221b5adbd40045e50cf46ff2cf7748e5cc
mingjingtang <tmjcsh@gmail.com>
2021-01-24 01:40:11 -0500

    fix two form.js and dailyReading.js conflict, success use login conditional render

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./App.css";
 import DailyReading from "./components/dailyReading";
-import Form from "./components/Form";
+import "./components/Form.css";
+import FormSignup from "./components/FormSignup";
 
 class App extends React.Component {
   state = {
@@ -11,12 +12,32 @@ class App extends React.Component {
 
   //   async componentDidMount() {}
 
+  submitForm() {
+    this.setState({ isLoggedIn: true });
+  }
+
   render() {
     const { isLoggedIn } = this.state;
 
     return (
       <main>
-        {!isLoggedIn ? <Form isSubmmited={isLoggedIn} /> : <DailyReading />}
+        {!isLoggedIn ? (
+          <div>
+            <div className="form-container">
+              <span className="close-btn">×</span>
+              <div className="form-content-left">
+                <img
+                  className="form-img"
+                  src="https://www.myhealthtalent.com/getasset/a4fb6627-ce16-4259-acdb-552f4af40c6b/"
+                  alt="spaceship"
+                />
+              </div>
+              <FormSignup submitForm={() => this.submitForm()} />
+            </div>{" "}
+          </div>
+        ) : (
+          <DailyReading />
+        )}
       </main>
     );
   }
